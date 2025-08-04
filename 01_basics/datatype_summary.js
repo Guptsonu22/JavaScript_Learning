2f0b665171e18d3160f045d34d7d45984bab09d7
Sonu Kumar <sonugupta411093@gmail.com>
2025-08-04 15:36:25 +0530

    datatype_summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,26 @@
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123')
+ const anotherId = Symbol('123')
+
+ console.log(id == anotherId);
+
+//  const bigNumber = 23543568756122n
+
+ const heros = ["shaktiman","naagraj", "doga"]
+  const user = {
+    name: "Sonu",
+    age: 22,
+ }
+
+ const myfunction = function() {
+    console.log("Hello world");
+ }
+
+ console.log(typeof bigNumber);
+ console.log(typeof outsideTemp);
